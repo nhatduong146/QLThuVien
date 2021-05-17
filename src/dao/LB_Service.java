751e76966441ed3ac17930c03ff4368bf3f7bf1f
unknown <nhatduong146@gmail.com>
2021-05-17 14:37:42 +0700

    Them 2 package model va dao

--- src/dao/LB_Service.java
+++ src/dao/LB_Service.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package views;
+package dao;
 
+import model.LB_User;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
